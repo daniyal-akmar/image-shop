c4919115ae1602e3058feae2abb059ac480096a9
daniyaal-akmar <daniyal.khan.akmar@gmail.com>
2023-01-02 11:39:08 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div className="container">
+            <h1>Hello, World</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, reiciendis dicta nobis officia optio ipsam mollitia nam deleniti temporibus? Ipsam.</p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur excepturi explicabo est illum deleniti asperiores quisquam consectetur repellendus ratione natus voluptatem,
+                architecto libero iure magnam quo aperiam eum veritatis commodi.
+            </p>
+        </div>
+    );
+};
+
+export default App;
